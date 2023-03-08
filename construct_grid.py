21d399097b2f296e2c0dbe3c5b852c2ac880a0b7
Nico Hieke <nico.hieke@gmail.com>
2023-03-08 17:51:18 +0100

    created all the functions needed to do the polygon and land-use feature matching using centroids for a given input city - needs to be fully synched and debugged though

--- construct_grid.py
+++ construct_grid.py
@@ -37,6 +37,85 @@ def make_grid (polygon, cell_size):
     grid_cut = gpd.sjoin(grid, polygon_meter)
     return grid_cut[['geometry']].reset_index(drop=True)
 
+
+#outputs a tuple with squared grid polygons, and respective centroids
+def make_grid_centroid(polygon, cell_size):
+    polygon_meter = polygon.set_crs(epsg=4326)  # Set the current CRS to WGS84
+    polygon_meter = polygon.to_crs(epsg=3035)  # Convert to the desired CRS (UTM zone 33N)
+    min_x, min_y, max_x, max_y = polygon_meter.bounds.values[0] #Retrieve extremes
+
+    num_cols = int((max_x - min_x) / cell_size)
+    num_rows = int((max_y - min_y) / cell_size)
+
+    polygons = []
+    centroids = []
+    for i in range(num_rows):
+        for j in range(num_cols):
+            x = min_x + j * cell_size + cell_size / 2
+            y = min_y + i * cell_size + cell_size / 2
+            centroid = Point(x, y)
+            centroids.append((centroid.x, centroid.y))
+            square = create_square_polygon(cell_size, centroid)
+            polygons.append(square)
+
+    grid = gpd.GeoDataFrame(geometry=polygons, crs='epsg:3035')
+    grid_cut = gpd.sjoin(grid, polygon_meter)
+    return grid_cut[['geometry']].reset_index(drop=True), centroids
+
+
+def create_centroids_gdf(polygon, cell_size):
+    # Create a grid of square polygons within the input polygon
+    grid_cut_centroids = make_grid_centroid(polygon, cell_size)
+
+    # Get the list of centroids from the output of make_grid_centroid()
+    centroids = grid_cut_centroids[1]
+
+    # Convert the list of centroids to a list of shapely Point objects
+    points = [Point(x, y) for x, y in centroids]
+
+    # Create a GeoDataFrame from the points
+    centroids_gdf = gpd.GeoDataFrame({'geometry': points})
+
+    # Set the CRS to epsg=3035
+    centroids_gdf = centroids_gdf.set_crs(epsg=3035)
+
+    return centroids_gdf
+
+def join_landuse_city(city_gdf, landuse_gdf):
+    # Join the landuse feature with the city polygon
+    landuse_joined = gpd.sjoin(landuse_gdf, city_gdf.to_crs(epsg=3035), predicate='within')
+
+    # Drop unnecessary columns
+    landuse_joined.drop(columns=['country', 'fua_name','prod_date','identifier','perimeter','comment', 'Pop2018', 'index_right', 'GID_3', 'GID_0',
+                                 'COUNTRY', 'GID_1', 'NAME_1', 'NL_NAME_1', 'GID_2', 'NAME_2', 'NL_NAME_2', 'NAME_3', 'VARNAME_3',
+                                 'NL_NAME_3', 'TYPE_3', 'ENGTYPE_3', 'CC_3', 'HASC_3', 'index__right','fua_code'], inplace=True)
+
+    return landuse_joined
+
+def join_landuse_centroid(city_gdf, landuse_gdf, cell_size):
+    # Create a GeoDataFrame of centroids for the input city polygon
+    centroids_gdf = create_centroids_gdf(city_gdf, cell_size)
+
+    # Match the centroid points with the land use data
+    landuse_centroid = gpd.sjoin(centroids_gdf, landuse_gdf, lsuffix='_left', rsuffix='_right')
+
+    return landuse_centroid
+
+from shapely.geometry import Polygon
+
+def points_to_squares(gdf, cell_size):
+    polygons = []
+    for point in gdf.geometry:
+        x, y = point.x, point.y
+        half_side = cell_size / 2
+        square = Polygon([(x - half_side, y - half_side),
+                          (x - half_side, y + half_side),
+                          (x + half_side, y + half_side),
+                          (x + half_side, y - half_side)])
+        polygons.append(square)
+    return gpd.GeoDataFrame(gdf.drop('geometry', axis=1), geometry=polygons, crs=gdf.crs)
+
+
 if __name__ == '__main__':
     df = gpd.read_file('../raw_data/gadm41_DEU_3.json')
     polygon_2 = gpd.GeoDataFrame(df[df['NAME_3']=='Dresden']['geometry'])
